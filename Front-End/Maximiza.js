bcd19aec8fdb68fe73af2ff8890958548fa819ee
pxavier0 <hpaulo406@gmail.com>
2019-05-20 09:09:11 -0300

    Create Maximiza.js

---
+++ Front-End/Maximiza.js
@@ -0,0 +1,20 @@
+$('#myCarousel').carousel({
+  interval: 5000
+})
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
